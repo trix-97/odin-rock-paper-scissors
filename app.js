d06001817ea4d03710f1ffd6233a53329fec6f4c
trix-97 <trix-97@outlook.com>
2024-01-19 15:20:11 +0530

    Create playRound function

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
-    console.log(Math.round(Math.random() * choices.length));
+    return console.log(Math.round(Math.random() * choices.length));
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        alert("Round Tie");
+    }
+    else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            alert(`Computer wins round!, ${computerSelection} beats ${playerSelection}`);
+        }
+        else if (computerSelection === "scissors") {
+            alert(`Player wins round!, ${playRound} beats ${computerSelection}`);
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
